a2bc527c6e98226cf496b416ba02c9d3e948c49e
alexpaul <alexpaulnyc@gmail.com>
2018-12-06 13:24:47 -0500

    - added movie image and gcd processing

--- MovieSearch/MovieSearch/Controllers/MovieSearchController.swift
+++ MovieSearch/MovieSearch/Controllers/MovieSearchController.swift
@@ -26,6 +26,7 @@ class MovieSearchController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     tableView.dataSource = self
+    tableView.delegate = self
     searchBar.delegate = self
   }
   
@@ -46,12 +47,63 @@ extension MovieSearchController: UITableViewDataSource {
     let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath)
     let movie = movies[indexPath.row]
     cell.textLabel?.text = movie.trackName
+    if let description = movie.longDescription {
+      cell.detailTextLabel?.text = description
+    }
+    
+    
+    // image processing is blocking the main thread
+//    do {
+//      let data = try Data(contentsOf: movie.artworkUrl100)
+//      cell.imageView?.image = UIImage(data: data)
+//    } catch {
+//      print("retrieving image data error: \(error)")
+//    }
+    
+    // using concurrency via GCD and DispatchQueue
+    // note: we should now have a placeholder image and activity indicator which will lead to
+    // creating a custom cell to be more flexible with where we locate the indicator
+//    let queue = DispatchQueue(label: "imageProcessing")
+//    queue.async {
+//      do {
+//        let data = try Data(contentsOf: movie.artworkUrl100)
+//        // back to the main thread to update the UI
+//        DispatchQueue.main.async {
+//          cell.imageView?.image = UIImage(data: data)
+//        }
+//      } catch {
+//        print("retrieving image data error: \(error)")
+//      }
+//    }
+    
+    // using quality of service classes
+    DispatchQueue.global(qos: .userInitiated).async {
+      do {
+        let data = try Data(contentsOf: movie.artworkUrl100)
+        DispatchQueue.main.async {
+          cell.imageView?.image = UIImage(data: data)
+        }
+      } catch {
+        print("retrieving image data error: \(error)")
+      }
+    }
+    
+    
     return cell
+  }
+}
+
+extension MovieSearchController: UITableViewDelegate {
+  func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+    return 120
   }
 }
 
 extension MovieSearchController: UISearchBarDelegate {
   func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+    // clear cache
+    URLCache.shared.removeAllCachedResponses()
+    
     searchBar.resignFirstResponder()
     guard let _ = searchBar.text?.isEmpty,
       let searchText = searchBar.text?.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed)
